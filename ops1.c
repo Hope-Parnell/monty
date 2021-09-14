e210fca26abf6709abcc60fa81b2bfd5f3cdaf67
Hope-Parnell <parnell.hope.c@gmail.com>
2021-09-14 16:23:22 +0000

    make Betty happy

--- ops1.c
+++ ops1.c
@@ -17,9 +17,9 @@ void pushOp(stack_t **stack, unsigned int line_number)
 		return;
 	}
 	for (n = 0; opCommand[1][n] != '\0'; n++)
-	{/*check for non-number input*/
-		if (opCommand[1][n] == '-' || opCommand[1][n] == '-')
-		 n++;/*go to next character to check digit*/
+	{
+		if ((opCommand[1][n] == '-') && (opCommand[1][n] == '.'))
+			n++;/*go to next char to check for a digit*/
 		if ((opCommand[1][n] < '0') && (opCommand[1][n] > '9'))
 		{
 			dprintf(STDERR_FILENO, "L%u: usage: push integer\n", line_number);
